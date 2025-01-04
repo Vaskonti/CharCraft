ef73eef9222d41569e85f729631a763d90efcedd
Alexander Makedonski <alemak2002@gmail.com>
2025-01-04 17:22:18 +0200

    Added image modifier options.

--- public/js/image_converter.js
+++ public/js/image_converter.js
@@ -3,31 +3,53 @@ import { asciiVisibilityRank, emptyCharacter } from '../../src/js/utils.js';
 import { Board } from './board.js'
  
 export class ImageConverter {
-    static parseImageToBoard(img, darkCharacterTreshold = 0, ResolutionX = null, ResolutionY = null) {
+    static parseImageToBoard(img, options = new ImageParseOptions()) {
 
         if (!img || !img.width || !img.height) {
             throw new Error("Invalid image provided.");
         }
 
-        if (!ResolutionX)
-        {
-            ResolutionX = img.width;
-        }
-        if (!ResolutionY)
-        {
-            ResolutionY = img.height;
-        }
+        options.resolutionX = options.resolutionX || img.width;
+        options.resolutionY = options.resolutionY || img.height;
 
-        const pixels = ImageConverter.getImagePixels(img, ResolutionX, ResolutionY);
+        const pixels = ImageConverter.getImagePixels(img, options);
+        ImageConverter.applyBrightnessFactor(pixels, options);
+        const board = ImageConverter.#createBoardFromPixels(pixels, options);
+        return board;
+    }
+
+    static #createBoardFromPixels(pixels, options) {
         const board = new Board(pixels.height, pixels.width);
         for (let i = 0; i < board.boardMatrix.length; i += 1) {
             for (let j = 0; j < board.boardMatrix[0].length; j += 1) {
                 const pixel = ImageConverter.getPixel(pixels, i, j);
-                board.boardMatrix[i][j].character = ImageConverter.getPixelSymbol(pixel, darkCharacterTreshold);
+                board.boardMatrix[i][j].character = ImageConverter.getPixelSymbol(pixel, options);
                 board.boardMatrix[i][j].color = ImageConverter.getPixelColorHex(pixel);
             }
         }
         return board;
+    }
+
+    static applyBrightnessFactor(pixels, options) {
+        if (options.brightnessFactor == 1.0) {
+            return;
+        }
+        for (let i = 0; i < pixels.data.length; i += 4) {
+            pixels.data[i] = Math.min(pixels.data[i] * options.brightnessFactor, 255);
+            pixels.data[i + 1] = Math.min(pixels.data[i + 1] * options.brightnessFactor, 255);
+            pixels.data[i + 2] = Math.min(pixels.data[i + 2] * options.brightnessFactor, 255);
+        }
+    }
+
+    static applyGammaCorrection(pixels, options) {
+        if (options.gammaCorrection == 1.0) {
+            return;
+        }
+        for (let i = 0; i < pixels.data.length; i += 4) {
+            pixels.data[i] = Math.min(Math.pow(pixels.data[i] / 255, gamma) * 255, 255);
+            pixels.data[i + 1] = Math.min(Math.pow(pixels.data[i + 1] / 255, gamma) * 255, 255);
+            pixels.data[i + 2] = Math.min(Math.pow(pixels.data[i + 2] / 255, gamma) * 255, 255);
+        }
     }
 
     /* returns values of pixel in 0-counted row, col */
@@ -47,13 +69,13 @@ export class ImageConverter {
         return pixel;
     }
 
-    static getImagePixels(img, resolutionX, resolutionY) {
+    static getImagePixels(img, options) {
         const canvas = document.createElement('canvas');
         const ctx = canvas.getContext('2d');
     
         // Determine the scaling factor based on the larger dimension to preserve the aspect ratio
-        const scaleX = resolutionX / img.width;
-        const scaleY = resolutionY / img.height;
+        const scaleX = options.resolutionX / img.width;
+        const scaleY = options.resolutionY / img.height;
         const scale = Math.min(scaleX, scaleY);
 
         const drawWidth = img.width * scale;
@@ -65,18 +87,19 @@ export class ImageConverter {
         return pixels;
     }
 
-    static getPixelSymbol(pixel, darkCharacterTreshold = 0) {
+    static getPixelSymbol(pixel, options) {
         const alpha = pixel.alpha; // TODO: consider alpha in some images
         if (alpha === 0) return emptyCharacter;
 
         const averageColor = (pixel.red + pixel.green + pixel.blue)/3;
         
-        if (darkCharacterTreshold > averageColor)
+        if (options.darkCharacterTreshold > averageColor)
         {
             return emptyCharacter;
         }
 
-        const characterIndex = Math.floor((averageColor / 255) * asciiVisibilityRank.length);
+        const index = Math.floor((averageColor / 255) * asciiVisibilityRank.length) + options.staticVolumeIncrease;
+        const characterIndex = Math.min(asciiVisibilityRank.length - 1, index);
         const character = asciiVisibilityRank[characterIndex];
 
         return character;
@@ -87,5 +110,22 @@ export class ImageConverter {
         const green = pixel.green.toString(16).padStart(2, '0');
         const blue = pixel.blue.toString(16).padStart(2, '0');
         return `#${red}${green}${blue}`;
+    }
+}
+
+
+export class ImageParseOptions {
+    constructor(darkCharacterTreshold = 0,
+                staticVolumeIncrease = 0,
+                brightnessFactor = 1.0,
+                gammaCorrection = 1.0,
+                resolutionX = null,
+                resolutionY = null) {
+        this.darkCharacterTreshold = darkCharacterTreshold;
+        this.staticVolumeIncrease = staticVolumeIncrease;
+        this.brightnessFactor = brightnessFactor;
+        this.gammaCorrection = gammaCorrection;
+        this.resolutionX = resolutionX;
+        this.resolutionY = resolutionY;
     }
 }
