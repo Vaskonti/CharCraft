3242cbc4914bb50ac060da95d7e24c65c25be424
Vaskonti <vasil.hristov@lab08.com>
2025-01-08 18:07:28 +0200

    [setup-BE-1] added env, logging and config management

--- backend/config/config.php
+++ backend/config/config.php
@@ -1,1 +1,36 @@
 <?php
+require_once "env.php";
+/**
+ * @throws Exception
+ */
+function config($key):array|string|null
+{
+    try {
+        static $configCache = [];
+
+        list($file, $setting) = explode('.', $key, 2);
+
+        if (!isset($configCache[$file])) {
+            $filePath = __DIR__ . "/{$file}.php";
+
+            if (!file_exists($filePath)) {
+                throw new Exception("Config file '{$file}.php' not found");
+            }
+
+            $configCache[$file] = require $filePath;
+        }
+
+        if (!isset($setting)) {
+            return $configCache[$file] ?? null;
+        }
+
+        if (!isset($configCache[$file][$setting])) {
+            throw new Exception("Config setting '{$setting}' not found in '{$file}.php'");
+        }
+
+        return $configCache[$file][$setting];
+    } catch (Exception $exception) {
+        log_issue($exception);
+        return null;
+    }
+}
