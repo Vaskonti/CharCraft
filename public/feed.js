ffdf6c6b6fa3923bc4ea39396b76c5f978198dca
genipami <pamivladi@gmail.com>
2025-01-22 13:04:01 +0200

    added dynamic html generation of the posts.

--- public/feed.js
+++ public/feed.js
@@ -1,13 +1,47 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const likeIcons = document.querySelectorAll(".like-icon");
+    const postsContainer = document.getElementById("posts-container");
+    const postsData = [
+        {
+            "user": "John Doe",
+            "time": "12:30, 20/01/2025",
+            "image": "/public/assets/images/logo.png",
+            "title": "Beautiful Sunset",
+            "likes": 98,
+            "description": "Vestibulum auctor dapibus neque, eu efficitur lectus pharetra et."
+        }
+    ];
 
-    likeIcons.forEach(icon => {
-        icon.addEventListener("click", function () {
-            if (icon.src.includes("heart-empty.png")) {
-                icon.src = "/public/assets/images/icons/heart-full.png";
-            } else {
-                icon.src = "/public/assets/images/icons/heart-empty.png";
-            }
+    function generatePosts(posts) {
+        posts.forEach(post => {
+            const postElement = document.createElement("section");
+            postElement.classList.add("post");
+
+    
+            postElement.innerHTML = `
+                <h3 class="user">${post.user}</h3>
+                <time class="created-at">${post.time}</time>
+                <img src="${post.image}" alt="ASCII image" class="ASCII-image">
+                <h3 class="title">${post.title}</h3>
+                <section class="likes">
+                    <img src="/public/assets/images/icons/heart-empty.png" 
+                         alt="like" 
+                         class="icon like-icon">
+                    <p>${post.likes}</p>
+                </section>
+                <p class="description">${post.description}</p>`
+            ;
+
+            const likeIcon = postElement.querySelector(".like-icon");
+            likeIcon.addEventListener("click", function () {
+                if (likeIcon.src.includes("heart-empty.png")) {
+                    likeIcon.src = "/public/assets/images/icons/heart-full.png";
+                } else {
+                    likeIcon.src = "/public/assets/images/icons/heart-empty.png";
+                }
+            });
+            postsContainer.appendChild(postElement);
         });
-    });
+    }
+    generatePosts(postsData);
+
 });
