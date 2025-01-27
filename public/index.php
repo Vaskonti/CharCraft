500b8a8c93f2ef6187c1d2f39809b2466dfe8cb1
Vasil <vortexblast550@gmail.com>
2025-01-27 18:30:10 +0200

    remove sqlite shit

--- public/index.php
+++ public/index.php
@@ -13,7 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
-    http_response_code(200); // Send HTTP 200 OK
+    http_response_code(200);
     exit;
 }
 if ($_SERVER['REQUEST_METHOD'] === 'POST' || $_SERVER['REQUEST_METHOD'] === 'GET') {
