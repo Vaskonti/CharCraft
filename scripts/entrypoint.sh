f127205dfe7015c454393e3218922d651285cc55
Vasil <vortexblast550@gmail.com>
2025-01-27 22:54:28 +0200

    [docker] finally docker is working correctly

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#set -e
-#apt-get install
-#cp .env.example .env
-#composer install
-#./scripts/run_migrations.sh
-#nginx -g "daemon off;"
+php /var/www/html/migrations/up/0001_create_migrations_log_table.php
+php /var/www/html/migrations/up/0002_create_tables.php
+php /var/www/html/migrations/up/0003_add_dummy_data.php
